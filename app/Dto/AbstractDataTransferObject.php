9cd3af469093ef23b01a9982fb61bb2abc8c18c9
PositiveInsu <positiveinsu@gmail.com>
2023-04-05 13:50:52 -0500

    Implement AbstractDTO and DTOInterface
    Create and arrangement the DTO class first using by legacy code.

--- app/Dto/AbstractDataTransferObject.php
+++ app/Dto/AbstractDataTransferObject.php
@@ -2,9 +2,14 @@
 
 namespace App\Dto;
 
+use Error;
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
-use PhpParser\Node\Expr\AssignOp\Mod;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use ReflectionClass;
+use ReflectionProperty;
 
 /**
  * @note
@@ -13,13 +18,14 @@ use PhpParser\Node\Expr\AssignOp\Mod;
 abstract class AbstractDataTransferObject implements DTOInterface
 {
     const JAVASCRIPT_UNDERSCORE = '_';
-    private bool $_isDataFromHTTPRequest = false;
+    private bool $isDataFromHTTPRequest = false;
     public Model|null $original_model_obj = null;
 
     public static function make(): static
     {
         return new static();
     }
+
     public static function makeWith(Model|array|Request $data): static
     {
         $dto = self::make()->setDataFrom($data);
@@ -31,41 +37,262 @@ abstract class AbstractDataTransferObject implements DTOInterface
         return $dto;
     }
 
+    public function setDataFrom(Model|array|Request $target): static
+    {
+        $this->checkDataOrigin($target);
+        $dataList = self::getDataArrayFrom($target);
+
+        return $this->setDataFromArray($dataList);
+    }
+
+    private function getDataArrayFrom(Model|array|Request $data): array
+    {
+        if (is_array($data)) {
+            $arrayData = $data;
+        } else {
+            $arrayData = $data->toArray();
+        }
+
+        return $arrayData;
+    }
+
+    public function getOriginalModel(): Model|null
+    {
+        return $this->original_model_obj;
+    }
+
+    public function toArrayForModel(): array
+    {
+        $valueList = $this->getOnlyScalarTypeValueList();
+        return $this->filteringByProperVariables($valueList);
+    }
+
+    public function toArray(): array
+    {
+        return $this->removeNullValueFromArray($this->getPrivateVariableValueList());
+    }
+
+    public function toJson(): string
+    {
+        return json_encode($this->getPrivateVariableValueList());
+    }
+
     private function setOriginalModel(Model $data): void
     {
         $this->original_model_obj = $data;
     }
 
-    public function setDataFrom(Model|array|Request $target): static
+    private function checkDataOrigin(Model|Request|array $target): void
     {
-        // TODO: Implement setDataFrom() method.
+        if ($target instanceof Request) {
+            $this->isDataFromHTTPRequest = true;
+        }
     }
 
-    public function getDataArrayFrom(Model|array|Request $data): array
+    private function setDataFromArray(array $dataList): static
     {
-        // TODO: Implement getDataArrayFrom() method.
+        $voVariableList = $this->filteringByProperVariables($this->getPrivateVariableList());
+
+        foreach ($voVariableList as $variableName => $defaultValue) {
+            $value = $this->getValueFromDataList($dataList, $variableName);
+
+            if ($this->isNotNull($value)) {
+                $this->executeSetMethodWith($variableName, $value);
+            }
+        }
+        return $this;
     }
 
-    public function getOriginalModel(): Model|null
+    /**
+     * Get private variable name list from DTO Object for further usage.
+     *
+     * I recommend to use the getter, setter patterns in the DTO object
+     * since data transfer object can secure data with only accepting setter or getter method not directly change.
+     * I set the variable name as a key of array then further method will set the value depends on the variable name.
+     *
+     * @return array<string, null>;
+     */
+    private function getPrivateVariableList(): array
     {
-        // TODO: Implement getOriginalModel() method.
+        $privateVariableList = [];
+        $reflect = new ReflectionClass(new static());
+        $properties = $reflect->getProperties(ReflectionProperty::IS_PRIVATE);
+
+        foreach ($properties as $property) {
+            $privateVariableList[$property->name] = null;
+        }
+
+        return $privateVariableList;
     }
 
-    public function toArrayForModel(): array
+    /**
+     * @param array<string, null> $valueList
+     * @return array<string, null>
+     */
+    private function filteringByProperVariables(array $valueList): array
     {
-        // TODO: Implement toArrayForModel() method.
+        return array_filter($valueList, function ($key) {
+            return $this->isProperVariableForModel($key);
+        }, ARRAY_FILTER_USE_KEY);
     }
 
-    public function toArray(): array
+    /**
+     * Check the data from JavaScript object which use the underscore variables.
+     *
+     * This method validate that data from JavaScript object which consist public variable.
+     * If JavaScript object hold the private underscore variable, in this case we skip that variables.
+     * So to send the data from the front end javascript object, it is better to change
+     * private variables to public variables.
+     *
+     * @param string $variableName
+     * @return bool
+     */
+    private function isProperVariableForModel(string $variableName): bool
     {
-        // TODO: Implement toArray() method.
+        $delimiter = substr($variableName, 0, 1);
+
+        if ($delimiter === $this::JAVASCRIPT_UNDERSCORE) {
+            return false;
+        }
+
+        return true;
     }
 
-    public function toJson(): string
+    private function getValueFromDataList(array $dataList, string $variableName): mixed
     {
-        // TODO: Implement toJson() method.
+        $value = Arr::get($dataList, $variableName);
+
+        if ($this->isNeedToCheckUnderscoreVariableName($value)) {
+            $underscoreVariableName = $this::JAVASCRIPT_UNDERSCORE . $variableName;
+            $value = Arr::get($dataList, $underscoreVariableName);
+        }
+
+        return $value;
     }
 
+    private function isNeedToCheckUnderscoreVariableName(mixed $value): bool
+    {
+        return $this->isDataFromHTTPRequest && is_null($value);
+    }
 
+    private function executeSetMethodWith(string $variableName, mixed $value): void
+    {
+        $setMethodName = $this->combineSetMethodName($variableName);
+        if (method_exists($this, $setMethodName)) {
+            call_user_func([$this, $setMethodName], $value);
+        }
+    }
 
+    private function getOnlyScalarTypeValueList(): array
+    {
+        $variableList = $this->getPrivateVariableValueList();
+        return array_filter($variableList, function ($value) {
+            return is_scalar($value);
+        });
+    }
+
+    private function getPrivateVariableValueList(): array
+    {
+        $privateVariableValueList = [];
+        foreach ($this->getPrivateVariableList() as $variableName => $value) {
+            $privateVariableValueList[$variableName] = $this->getPropertyValue($variableName);
+        }
+        return $privateVariableValueList;
+    }
+
+    private function getPropertyValue(string $name): mixed
+    {
+        $value = null;
+        $getMethodName = $this->combineGetMethodName($name);
+        $booleanGetMethodName = $this->combineBooleanGetMethodName($name);
+
+        if (method_exists($this, $getMethodName)) {
+            $value = $this->callMethod($getMethodName);
+        } elseif (method_exists($this, $booleanGetMethodName)) {
+            $value = $this->callMethod($booleanGetMethodName);
+        }
+
+        return $value;
+    }
+
+    private function removeNullValueFromArray(array $variableList): array
+    {
+        $removedNullValueList = [];
+
+        foreach ($variableList as $variableName => $value) {
+            if ($this->isNotNull($value)) {
+                switch (gettype($value)) {
+                    case 'object':
+                        $removedNullValueList[$variableName] = $this->getValueListWhenObjectType($value);
+                        break;
+                    case 'array':
+                        if ($this->hasValueInArray($value)) {
+                            $removedNullValueList[$variableName] = $this->removeNullValueFromArray($value);
+                        }
+                        break;
+                    default:
+                        $removedNullValueList[$variableName] = $value;
+                        break;
+                }
+            }
+        }
+
+        return $removedNullValueList;
+    }
+
+    private function getArrayAfterRemovingNullValue(object $target): array
+    {
+        $variableList = get_object_vars($target);
+        return $this->removeNullValueFromArray($variableList);
+    }
+
+    private function getValueListWhenObjectType(object $value): array
+    {
+        if ($value instanceof DTOInterface) {
+            $valueListAfterRemovedNullValue = $this->removeNullValueFromArray($value->toArray());
+        } else {
+            $valueListAfterRemovedNullValue = $this->getArrayAfterRemovingNullValue($value);
+        }
+
+        return $valueListAfterRemovedNullValue;
+    }
+
+    private function combineSetMethodName(string $variableName): string
+    {
+        return 'set' . ucfirst(Str::camel($variableName));
+    }
+
+    private function combineGetMethodName(string $variableName): string
+    {
+        return 'get' . ucfirst(Str::camel($variableName));
+    }
+
+    private function combineBooleanGetMethodName(string $variableName): string
+    {
+        return 'is' . ucfirst(Str::camel($variableName));
+    }
+
+    private function callMethod(string $methodName): mixed
+    {
+        $value = null;
+        try {
+            $value = call_user_func([$this, $methodName]);
+        } catch (Exception|Error $e) {
+            /**
+             * This is the noticeable Exception or error, so I ignore the Exception or Error's.
+             * The developer can know easily that setting data failed because DTO didn't hold the data.
+             */
+        }
+        return $value;
+    }
+
+    private function isNotNull(mixed $value): bool
+    {
+        return !is_null($value);
+    }
+
+    private function hasValueInArray(array $value): bool
+    {
+        return !empty($value);
+    }
 }
--- app/Dto/DTOInterface.php
+++ app/Dto/DTOInterface.php
@@ -1,17 +1,74 @@
 <?php
 
 namespace App\Dto;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
 interface DTOInterface
 {
+    /**
+     * Create an empty DTO Object.
+     *
+     * Use this method when need the empty DTO object in order to Save the data
+     *
+     * @return static
+     */
     public static function make(): static;
+
+    /**
+     * Create new DTO object with given data
+     *
+     * Use this method changing the data from Model, array or Request to DTO Object.
+     * Only
+     *
+     * @param Model|Request|array $data
+     *
+     * @return static
+     */
     public static function makeWith(Model|Request|array $data): static;
+
+    /**
+     * Just set the Data from Model, Array, Request to DTO object already exist.
+     *
+     * If there is DTO object which is already made, set the data to that DTO object.
+     *
+     * @param Model|Request|array $target
+     * @return static
+     */
     public function setDataFrom(Model|Request|array $target): static;
-    public function getDataArrayFrom(Model|Request|array $data):array;
+
+    /**
+     * Get original Model object.
+     *
+     * When use makeWith() method, if $data is the Model object then DTO hold the original Model
+     * to use later; e.g. making relation with other model directly.
+     *
+     * @return Model|null
+     */
     public function getOriginalModel(): Model|null;
+
+    /**
+     * Get only Scalar data for creating Model object
+     *
+     * Data Transfer Object can hold the relational data with Array or other DTO object.
+     * (e.g. User DTO can hold the related Role DTO data.)
+     * In order to change the DTO data to array format to insert to the Model,
+     * I ignored any other relational data like array or object.
+     *
+     * The Model's fillable will distill the data, which is only acceptable.
+     */
     public function toArrayForModel(): array;
+
+    /**
+     * Get all the data with Array format.
+     *
+     * This method return not only Scalar data but also other array and object data
+     * in which DTO hold.
+     *
+     * @return array
+     */
     public function toArray(): array;
+
     public function toJson():string;
 }
